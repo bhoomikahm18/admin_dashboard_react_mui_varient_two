2132c704191790c4e5ae160f078ff5510633f170
Bhoomika <bhoomikahm18@gmail.com>
2023-12-21 15:07:27 +0530

    Adding profile form page

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,15 @@ import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
-// import Bar from "./scenes/bar.jsx";
-// import Form from "./scenes/form.jsx";
-// import Line from "./scenes/line.jsx";
-// import Pie from "./scenes/pie.jsx";
-// import FAQ from "./scenes/faq.jsx";
-// import Geography from "./scenes/geography.jsx";
+// import Bar from "./scenes/bar";
+import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme.jsx";
-// import Calendar from "./scenes/calendar/calendar.jsx";
+// import Calendar from "./scenes/calendar/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -33,8 +33,8 @@ function App() {
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
-              {/* <Route path="/form" element={<Form />} />
-              <Route path="/bar" element={<Bar />} />
+              <Route path="/form" element={<Form />} />
+              {/* <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
               <Route path="/faq" element={<FAQ />} />
