14a0335eb68200b52a11f747cb518fdf87328a12
Bhoomika <bhoomikahm18@gmail.com>
2023-12-21 14:25:45 +0530

    Adding contact page

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Sidebar from "./scenes/global/Sidebar.jsx";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 // import Invoices from "./scenes/invoices";
-// import Contacts from "./scenes/contacts";
+import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar.jsx";
 // import Form from "./scenes/form.jsx";
 // import Line from "./scenes/line.jsx";
@@ -31,8 +31,8 @@ function App() {
             <Routes>
              <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
-              {/*  <Route path="/contacts" element={<Contacts />} />
-              <Route path="/invoices" element={<Invoices />} />
+             <Route path="/contacts" element={<Contacts />} />
+               {/*  <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
