8940c8ed4a16b394456906fb97b38151198c74c0
Bhoomika <bhoomikahm18@gmail.com>
2023-12-21 14:41:29 +0530

    Adding Invoice balances

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Topbar from "./scenes/global/Topbar.jsx";
 import Sidebar from "./scenes/global/Sidebar.jsx";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
-// import Invoices from "./scenes/invoices";
+import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar.jsx";
 // import Form from "./scenes/form.jsx";
@@ -29,11 +29,11 @@ function App() {
           <main className="content">
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
-             <Route path="/" element={<Dashboard />} />
+              <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
-             <Route path="/contacts" element={<Contacts />} />
-               {/*  <Route path="/invoices" element={<Invoices />} />
-              <Route path="/form" element={<Form />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              {/* <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
