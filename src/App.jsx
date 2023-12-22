0acb32feda5786f8617cb64feb791b3bc5e9c747
Bhoomika <bhoomikahm18@gmail.com>
2023-12-22 11:53:02 +0530

    Adding Pie chart page

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Contacts from "./scenes/contacts";
 import Bar from "./scenes/bar";
 import Form from "./scenes/form";
 // import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
+import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
@@ -37,8 +37,8 @@ function App() {
               <Route path="/calendar" element={<Calendar />} />
               <Route path="/faq" element={<FAQ />} />
               <Route path="/bar" element={<Bar />} />
-              {/* <Route path="/pie" element={<Pie />} />
-              <Route path="/line" element={<Line />} />
+              <Route path="/pie" element={<Pie />} />
+              {/* <Route path="/line" element={<Line />} />
               <Route path="/geography" element={<Geography />} /> */}
             </Routes>
           </main>
